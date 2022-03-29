21957d24248b1ed09267e4ba79033715c356d038
Corpulent Brony <corpulent@brony.email>
2022-03-29 12:53:57 -0400

    Added status output to generateClaimCodes script

--- scripts/generateClaimCodes.js
+++ scripts/generateClaimCodes.js
@@ -7,21 +7,32 @@ const SilverMareCoinDCoA = artifacts.require("SilverMareCoinDCoA");
 function asyncDelay(delayTimeMs) { return new Promise(resolve => setTimeout(resolve, delayTimeMs)); }
 
 async function generateClaimCodes(callbackFn) {
-	const currentNetwork = await utils.getNetworkName(config);
-	const silverMareCoinDCoA = await SilverMareCoinDCoA.deployed();
-	const cap = await silverMareCoinDCoA.cap();
-	const certificateClaimCodes = new Array(cap);
+	try {
+		const currentNetwork = await utils.getNetworkName(config);
+		const silverMareCoinDCoA = await SilverMareCoinDCoA.deployed();
+		const cap = await silverMareCoinDCoA.cap();
+		const certificateClaimCodes = new Array(cap);
+		const generatingMessage = "Generating claim code ";
+		process.stdout.write(generatingMessage);
 
-	for (let i = 0; i < cap; i++) {
-		const number = i + 1;
-		const certificateSigningHash = await silverMareCoinDCoA.certificateSigningHash(number);
-		certificateClaimCodes[i] = { number, signature: await web3.eth.sign(certificateSigningHash, SIGNERS[currentNetwork]) };
+		for (let i = 0; i < cap; i++) {
+			const number = i + 1;
+			process.stdout.cursorTo(generatingMessage.length);
+			process.stdout.write(`${number}/${cap}...`);
+			const certificateSigningHash = await silverMareCoinDCoA.certificateSigningHash(number);
+			certificateClaimCodes[i] = { number, signature: await web3.eth.sign(certificateSigningHash, SIGNERS[currentNetwork]) };
 
-		if (number < cap && number % 10 === 0)
-			await asyncDelay(1000);
+			if (number < cap && number % 10 === 0)
+				await asyncDelay(1000);
+		}
+		process.stdout.write("\n");
+		console.log("Saving claim codes...")
+		await fs.writeFile(`claimCodes-${currentNetwork}.json`, JSON.stringify(certificateClaimCodes, null, "\t"));
+		console.log("Done!");
+		callbackFn();
+	} catch (e) {
+		callbackFn(e);
 	}
-	await fs.writeFile(`claimCodes-${currentNetwork}.json`, JSON.stringify(certificateClaimCodes, null, "\t"));
-	callbackFn();
 }
 
 module.exports = generateClaimCodes;
